92645019ef14d4d2c2de3f26ec8f6d6f27002768
kuanhc96 <kuanhc96@uchicago.edu>
2024-02-23 16:09:18 +0800

    add cnn model

--- pyimagesearch/models.py
+++ pyimagesearch/models.py
@@ -12,3 +12,35 @@ def create_mlp(dim, regress=False):
         model.add(Dense(1, activation="linear"))
 
     return model
+
+def create_cnn(width, height, depth, filters=(16, 32, 64), regress=False):
+    input_shape = (height, width, depth)
+    channel_dimension = -1
+    model = Sequential()
+
+    # CONV -> RELU -> BN -> POOL
+    # loop over filters:
+    for (i, f) in enumerate(filters):
+        # if this is the first CONV layer then set the input
+        if i == 0:
+            model.add(Conv2D(f, (3, 3), padding="same", input_dim=input_shape))
+        else:
+            model.add(Conv2D(f, (3, 3), padding="same"))
+
+        model.add(Activation("relu"))
+        model.add(BatchNormalization(axis=channel_dimension))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(16))
+    model.add(Activation("relu"))
+    model.add(BatchNormalization(axis=channel_dimension))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(4))
+    model.add(Activation("relu"))
+
+    if regress:
+        model.add(Dense(1, activation="linear"))
+
+    return model
